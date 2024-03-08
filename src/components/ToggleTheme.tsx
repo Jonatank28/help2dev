8527d63ea245a5601c76faf3edcd8fc1a50cfacf
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-03-08 08:05:02 -0300

    adjust2

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,22 +1,29 @@
 'use client'
 
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
+import { Button } from './ui/button'
 import { useTheme } from 'next-themes'
-import { Button } from '@/components/ui/button'
+import { useEffect, useState } from 'react'
 
 export function ToggleTheme() {
   const { setTheme, theme } = useTheme()
+  const [client, setClient] = useState(false)
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
+
+  useEffect(() => {
+    if (theme) return setClient(true)
+  }, [theme])
 
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-    >
-      {theme === 'dark' ? (
-        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      ) : (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      {client && theme == 'light' ? (
         <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      ) : (
+        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       )}
     </Button>
   )
--- src/context/editorContext.tsx
+++ src/context/editorContext.tsx
@@ -13,6 +13,20 @@ interface ThemeContextType {
   setFormattedJson: (value: string) => void
 }
 
+// Custom Hook for Local Storage
+function useLocalStorage(key: string, defaultValue: any) {
+  const [value, setValue] = useState(() => {
+    const saved = localStorage.getItem(key)
+    return saved ? JSON.parse(saved) : defaultValue
+  })
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
+
+  return [value, setValue]
+}
+
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 const EditorProvider = ({ children }: { children: React.ReactNode }) => {
@@ -25,24 +39,31 @@ const EditorProvider = ({ children }: { children: React.ReactNode }) => {
   const [jsonInput, setJsonInput] = useState('')
   const [formattedJson, setFormattedJson] = useState('')
 
+  const [themeStorage, setThemeStorage] = useLocalStorage('@help2dev', {
+    theme: {},
+  })
+
   const onChangeThemeEditor = (value: string) => {
     if (defaultThemes.includes(value)) {
-      if (theme === 'dark') {
-        localStorage.setItem('@themeDarkEditor', value)
-      } else {
-        localStorage.setItem('@themeLightEditor', value)
-      }
+      setThemeStorage((prev: any) => ({
+        ...prev,
+        theme: {
+          ...prev.theme,
+          [theme as string]: value,
+        },
+      }))
       setThemeSelected(value)
     }
   }
 
   useEffect(() => {
-    if (theme === 'dark') {
-      setThemeSelected(localStorage.getItem('@themeDarkEditor') || 'dracula')
+    const storedTheme = themeStorage.theme[theme as string] ?? null
+    if (!storedTheme) {
+      setThemeSelected(theme === 'dark' ? 'dracula' : 'chrome')
     } else {
-      setThemeSelected(localStorage.getItem('@themeLightEditor') || 'chrome')
+      setThemeSelected(storedTheme)
     }
-  }, [theme])
+  }, [theme, themeStorage])
 
   useEffect(() => {
     if (pathName !== '/json-formatter') {
