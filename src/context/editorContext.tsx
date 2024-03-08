f20daed95ef252b1b50eba863ea0d5628e440213
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-03-08 07:54:39 -0300

    refactor context

--- src/context/editorContext.tsx
+++ src/context/editorContext.tsx
@@ -13,20 +13,6 @@ interface ThemeContextType {
   setFormattedJson: (value: string) => void
 }
 
-// Custom Hook for Local Storage
-function useLocalStorage(key: string, defaultValue: any) {
-  const [value, setValue] = useState(() => {
-    const saved = localStorage.getItem(key)
-    return saved ? JSON.parse(saved) : defaultValue
-  })
-
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value))
-  }, [key, value])
-
-  return [value, setValue]
-}
-
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 const EditorProvider = ({ children }: { children: React.ReactNode }) => {
@@ -39,31 +25,24 @@ const EditorProvider = ({ children }: { children: React.ReactNode }) => {
   const [jsonInput, setJsonInput] = useState('')
   const [formattedJson, setFormattedJson] = useState('')
 
-  const [themeStorage, setThemeStorage] = useLocalStorage('@help2dev', {
-    theme: {},
-  })
-
   const onChangeThemeEditor = (value: string) => {
     if (defaultThemes.includes(value)) {
-      setThemeStorage((prev: any) => ({
-        ...prev,
-        theme: {
-          ...prev.theme,
-          [theme as string]: value,
-        },
-      }))
+      if (theme === 'dark') {
+        localStorage.setItem('@themeDarkEditor', value)
+      } else {
+        localStorage.setItem('@themeLightEditor', value)
+      }
       setThemeSelected(value)
     }
   }
 
   useEffect(() => {
-    const storedTheme = themeStorage.theme[theme as string] ?? null
-    if (!storedTheme) {
-      setThemeSelected(theme === 'dark' ? 'dracula' : 'chrome')
+    if (theme === 'dark') {
+      setThemeSelected(localStorage.getItem('@themeDarkEditor') || 'dracula')
     } else {
-      setThemeSelected(storedTheme)
+      setThemeSelected(localStorage.getItem('@themeLightEditor') || 'chrome')
     }
-  }, [theme, themeStorage])
+  }, [theme])
 
   useEffect(() => {
     if (pathName !== '/json-formatter') {
