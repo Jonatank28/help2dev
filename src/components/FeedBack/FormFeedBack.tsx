cf210f79cde49ace03b836c495882c9ec1c98b25
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-03-09 14:42:55 -0300

    add feedback email

--- src/components/FeedBack/FormFeedBack.tsx
+++ src/components/FeedBack/FormFeedBack.tsx
@@ -7,17 +7,22 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog'
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from '../ui/select'
+import { Input } from '../ui/input'
+import { Textarea } from '../ui/textarea'
+import { useEffect, useState } from 'react'
+import emailjs from 'emailjs-com'
+import { toast } from 'sonner'
+import useLoading from '@/hooks/useLoading'
+import { isValidEmail } from '@/lib/utils'
 
 interface Props {
   open: boolean
@@ -25,60 +30,128 @@ interface Props {
 }
 
 const FormFeedBack = ({ open, onClose }: Props) => {
-  const handleFormSubmit = (e: React.FormEvent) => {
+  const { isLoading, startLoading, stopLoading } = useLoading()
+  const [name, setName] = useState<null | string>(null)
+  const [email, setEmail] = useState<null | string>(null)
+  const [type, setType] = useState<null | string>(null)
+  const [message, setMessage] = useState<null | string>(null)
+
+  const resetForm = () => {
+    setName(null)
+    setEmail(null)
+    setType(null)
+    setMessage(null)
+  }
+
+  const sendEmail = async () => {
+    startLoading()
+    try {
+      await emailjs.send(
+        'service_k21iesm',
+        'template_3m4hvfq',
+        {
+          from_name: name,
+          from_email: email,
+          type: type,
+          message: message,
+        },
+        'HGj7UBc6tZzJNEO9g'
+      )
+      return true
+    } catch (error) {
+      return false
+    } finally {
+      stopLoading()
+    }
+  }
+
+  const handleFormSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    console.log('submit')
+    const send = await sendEmail()
+    if (!send) {
+      toast.error('Error, please try again')
+    } else {
+      onClose()
+      toast.success('Thank you for your feedback!')
+      resetForm()
+    }
   }
 
   const typeFeedBack = [
     {
       id: 1,
-      name: 'Dúvidas',
+      name: 'Doubt',
     },
     {
       id: 2,
-      name: 'Erro',
+      name: 'Error',
     },
     {
       id: 3,
-      name: 'Sugestão',
+      name: 'Suggestion',
     },
   ]
+
+  useEffect(() => {
+    resetForm()
+  }, [onClose])
 
   return (
     <AlertDialog open={open} onOpenChange={onClose}>
       <AlertDialogContent>
         <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogTitle>Tell me your experience</AlertDialogTitle>
           <AlertDialogDescription>
-            <p>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </p>
             {/* Type */}
-            <Select>
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Type" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Fruits</SelectLabel>
-                  {typeFeedBack.map((type) => {
-                    return (
-                      <SelectItem key={type.id} value={type.name}>
-                        {type.name}
-                      </SelectItem>
-                    )
-                  })}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
+            <div className="mt-4 space-y-2">
+              <Input
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <Input
+                placeholder="Email"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Select onValueChange={(e) => setType(e)}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Type feedback" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {typeFeedBack.map((type) => {
+                      return (
+                        <SelectItem key={type.id} value={type.name}>
+                          {type.name}
+                        </SelectItem>
+                      )
+                    })}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Textarea
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Type your message here."
+                className="border border-background-foreground resize-none"
+                rows={6}
+              />
+            </div>
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction onClick={handleFormSubmit} className="text-white">
-            Continue
+          <AlertDialogAction
+            onClick={handleFormSubmit}
+            className="text-white"
+            disabled={
+              !name ||
+              !isValidEmail(email || '') ||
+              !type ||
+              !message ||
+              isLoading
+            }
+          >
+            {isLoading ? 'Sending...' : 'Send'}
           </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
