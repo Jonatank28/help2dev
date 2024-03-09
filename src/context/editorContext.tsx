79d329118f295cc12c54dd70a14883475aa882f5
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-03-09 01:59:29 -0300

    fix theme

--- src/context/editorContext.tsx
+++ src/context/editorContext.tsx
@@ -26,49 +26,26 @@ const EditorProvider = ({ children }: { children: React.ReactNode }) => {
   const [formattedJson, setFormattedJson] = useState('')
 
   const onChangeThemeEditor = (value: string) => {
-    const getStorage = localStorage.getItem('@help2dev')
-    if (theme && defaultThemes.includes(value)) {
-      let temp
-      try {
-        temp = JSON.parse(getStorage || '{}') || {}
-        if (theme === 'dark') {
-          temp.jsonFormatter = {
-            ...temp.jsonFormatter,
-            theme: {
-              ...temp.jsonFormatter.theme,
-              dark: value,
-            },
-          }
-
-          localStorage.setItem('@help2dev', JSON.stringify(temp))
-        } else {
-          temp.jsonFormatter = {
-            ...temp.jsonFormatter,
-            theme: {
-              ...temp.jsonFormatter.theme,
-              light: value,
-            },
-          }
-          localStorage.setItem('@help2dev', JSON.stringify(temp))
-        }
-        setThemeSelected(value)
-      } catch (error) {
-        console.error('Error parsing localStorage data:', error)
-        temp = {}
+    if (defaultThemes.includes(value)) {
+      const temp = JSON.parse(localStorage.getItem('@help2dev') || '{}') || {}
+      temp.theme = {
+        ...temp.theme,
+        [theme === 'dark' ? 'dark' : 'light']: value,
       }
+      localStorage.setItem('@help2dev', JSON.stringify(temp))
+      setThemeSelected(value)
     }
   }
 
   useEffect(() => {
     const getStorage =
       JSON.parse(localStorage.getItem('@help2dev') || '{}') || {}
-    const defaltTheme = getStorage.jsonFormatter
-      ? getStorage.jsonFormatter.theme[theme as string] ?? null
-      : null
-    if (!defaltTheme) {
+    const defaultTheme =
+      getStorage.theme?.[theme === 'dark' ? 'dark' : 'light'] ?? null
+    if (!defaultTheme) {
       setThemeSelected(theme === 'dark' ? 'dracula' : 'chrome')
     } else {
-      setThemeSelected(defaltTheme)
+      setThemeSelected(defaultTheme)
     }
   }, [theme])
 
