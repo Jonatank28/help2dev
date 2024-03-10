7a679dc9a3330e817ddc5e5a1a17e0debd954608
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-03-10 09:29:47 -0300

    adjust

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,19 @@
 'use client'
 
 import { usePathname, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function NotFound() {
   const router = useRouter()
   const pathName = usePathname()
 
-  let temp = JSON.parse(localStorage.getItem('@help2dev') || '{}') || {}
-  const localeCurrent = (temp.locale = temp.locale || 'en')
-  localStorage.setItem('@help2dev', JSON.stringify(temp))
+  useEffect(() => {
+    let temp = JSON.parse(localStorage.getItem('@help2dev') || '{}') || {}
+    const localeCurrent = (temp.locale = temp.locale || 'en')
+    localStorage.setItem('@help2dev', JSON.stringify(temp))
 
-  return router.push(`/${localeCurrent}${pathName}`)
+    return router.push(`/${localeCurrent}${pathName}`)
+  }, [pathName, router])
+
+  return null
 }
