0df2b84c00de981a3e3b7de88d8d0283669adc81
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-03-12 20:07:30 -0300

    test adsense

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css'
 import { Inter as FontSans } from 'next/font/google'
 import { NextIntlClientProvider, useMessages } from 'next-intl'
 import { unstable_setRequestLocale } from 'next-intl/server'
+import Head from 'next/head'
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -28,6 +29,9 @@ export default function RootLayout({
   const messages = useMessages()
   return (
     <html lang={locale} suppressHydrationWarning>
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6708395486232861" crossOrigin="anonymous"></script>
+      </Head>
       <body
         className={cn(
           'min-h-screen font-sans antialiased scroll-smooth',
