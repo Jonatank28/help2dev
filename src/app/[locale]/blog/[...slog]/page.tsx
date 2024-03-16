a2157553da5ba89c0f398d5b0d50487eba18ea95
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-03-16 20:04:49 -0300

    settings logo

--- src/app/[locale]/blog/[...slog]/page.tsx
+++ src/app/[locale]/blog/[...slog]/page.tsx
@@ -5,7 +5,6 @@ const PageBlogDetail = ({ params }: any) => {
   return (
     <main>
       <div className="w-[min(1200px,95vw)] mx-auto">
-        <Image />
         <h1 className="text-3xl font-bold">React: Componentes e Propriedades</h1>
       </div>
     </main>
