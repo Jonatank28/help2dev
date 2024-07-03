0c7f55dfee5f998267128ad18d0704d324c48ec6
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-07-02 23:14:01 -0300

    rre

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,9 @@
 export const api =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/api/'
-    : 'https://help2dev.com/api/'
+    : 'https://help2dev-ia.vercel.app/api/'
 
 export const apiMovies =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://help2dev.com'
+    : 'https://help2dev-ia.vercel.app'
