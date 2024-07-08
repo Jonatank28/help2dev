dd876b6a3615d034175a4b478df2ae6c22971d18
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-07-08 17:03:27 -0300

    trtrrt

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,21 +4,25 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import Header from "./Header"
 import Footer from "./Footer"
 import { Toaster } from "./ui/toaster"
+import { GoogleAdSense } from "next-google-adsense";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem={false}
-    >
-      <Toaster />
-      <div className='min-h-screen grid grid-rows-[auto_1fr_auto]'>
-        <Header />
-        {children}
-        <Footer />
-      </div>
-    </NextThemesProvider >
+    <>
+      <GoogleAdSense publisherId="pub-6708395486232861" />
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem={false}
+      >
+        <Toaster />
+        <div className='min-h-screen grid grid-rows-[auto_1fr_auto]'>
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </NextThemesProvider>
+    </>
   )
 }
 
