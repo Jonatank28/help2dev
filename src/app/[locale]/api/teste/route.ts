126d6219ea4569ac661a7ce10cbe97159b455448
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-09-13 22:18:37 -0300

    66

--- src/app/[locale]/api/teste/route.ts
+++ src/app/[locale]/api/teste/route.ts
@@ -1,3 +1,10 @@
+import { movies } from "@/data/movies";
+
 export async function GET(request: Request) {
-  return new Response("Hello, Next.js!");
+  return new Response(JSON.stringify(movies), {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
 }
