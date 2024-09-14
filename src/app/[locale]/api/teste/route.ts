c00175e6e3127730789daf325f3160956df092ef
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-09-13 22:33:30 -0300

    887

--- src/app/[locale]/api/teste/route.ts
+++
@@ -1,7 +0,0 @@
-import { movies } from "@/data/movies";
-
-export async function GET(request: Request) {
-  return new Response(JSON.stringify(movies), {
-    status: 200,
-  });
-}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,9 @@
 export const api =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000/api/'
-    : 'https://help2dev-ia.vercel.app/api/'
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api/"
+    : "https://help2dev.com/en/api/";
 
 export const apiMovies =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://help2dev-ia.vercel.app'
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://help2dev.com";
