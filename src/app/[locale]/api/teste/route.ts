6229500afc968e666f5e7030915045d8e0a4b093
Jonatank28 <jonatankalmeidakk28@gmail.com>
2024-09-13 22:21:52 -0300

    77

--- src/app/[locale]/api/teste/route.ts
+++ src/app/[locale]/api/teste/route.ts
@@ -1,10 +1,5 @@
 import { movies } from "@/data/movies";
 
 export async function GET(request: Request) {
-  return new Response(JSON.stringify(movies), {
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+  return new Response(JSON.stringify(movies));
 }
