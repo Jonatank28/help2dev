e51fd4ce68d61c343420463537c46822811f4523
Jonatan S Almeida <90075235+Jonatank28@users.noreply.github.com>
2024-12-16 14:39:19 -0300

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,10 +5,29 @@ import Header from "./Header"
 import Footer from "./Footer"
 import { Toaster } from "./ui/toaster"
 import { GoogleAdSense } from "next-google-adsense";
+import Script from "next/script"
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const id = 'G-2E2WYW2QJ7'
   return (
     <>
+      {/* Google Analytics Script */}
+      <GoogleAnalytics key={id} />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${id}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <GoogleAdSense publisherId="pub-6708395486232861" />
       <NextThemesProvider
         attribute="class"
